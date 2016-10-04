594719d7c7145740bc4902750773b397fdfa6fd4
Egor Pinkovskiy <trushnick.dev@gmail.com>
2016-10-04 09:44:18 +0300

    ParserError + some wierd stuff. Thinking of parser design...

--- cstruct2xml/parser.py
+++ cstruct2xml/parser.py
@@ -19,6 +19,68 @@ class Parser:
 
     def __init__(self, lexer):
         self.lexer = lexer
+        self.current = next(lexer)
 
     def parse(self):
         pass
+
+    def _structure_definition(self):
+        pass
+
+    def _comment_block(self):
+        pass
+
+    def _comment(self):
+        pass
+
+    def _structure_body(self):
+        pass
+
+    def _inner_structure_definition(self):
+        pass
+
+    def _variable_declaration(self):
+        pass
+
+    def _variable_specification(self):
+        pass
+
+    def _variable_type(self):
+        pass
+
+    def _primitive_type(self):
+        pass
+
+    def _users_type(self):
+        pass
+
+    def _array_specifier(self):
+        pass
+
+    def _array_size(self):
+        pass
+
+    def _array_size_expression(self):
+        pass
+
+    def _array_size_expression2(self):
+        pass
+
+    def _array_size_expression3(self):
+        pass
+
+    def _structure_name(self):
+        pass
+
+    def _check_token(self, token_type):
+        if self.current.type == token_type:
+            return True
+        else:
+            raise ParserError
+
+
+class ParserError(Exception):
+
+    def __init__(self, token):
+        message = 'Wrong lexeme: {} at line {}, pos {}'.format(token, self.lexer.line_number, self.lexer.line_pos)
+        super(ParserError, self).__init__(message)
