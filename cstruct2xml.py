9ca14eacd155f0f032fbb18e8bdb4d70e514364a
Egor Pinkovskiy <trushnick.dev@gmail.com>
2017-03-23 11:53:24 +0300

    Refactored script

--- cstruct2xml.py
+++ cstruct2xml.py
@@ -11,10 +11,6 @@ from lib.convert import convert, convert_file
 import lib.resolver as resolver
 
 
-xsl = et.parse("file.xsl")
-transform = et.XSLT(xsl) # XSL transformation function
-
-
 def process_dir(dir_path):
     print("Processing directory: " + dir_path)
     for path in os.listdir(dir_path):
@@ -27,10 +23,10 @@ def process_dir(dir_path):
 
 def process_file(file_path):
     print("Processing: " + file_path + "...")
-    extractor = Extractor(file_path, _encoding)
+    extractor = Extractor(file_path, options.encoding)
     original_file_name = os.path.splitext(os.path.basename(file_path))[0]
     structures = []
-    root_dir_name = _output_directory if _output_directory \
+    root_dir_name = options.directory if options.directory \
         else os.path.join(os.path.dirname(file_path), 'cstruct2xml-output')
     if not os.path.exists(root_dir_name):
         os.mkdir(root_dir_name)
@@ -46,7 +42,7 @@ def process_file(file_path):
         else:
             print("Found structure {}".format(structure.name))
             structures.append(structure)
-            if not _one_output_per_file:
+            if not options.file_only:
                 xml = convert(structure)
                 dir_name = os.path.join(root_dir_name, original_file_name)
                 if not os.path.exists(dir_name):
@@ -63,45 +59,40 @@ def process_file(file_path):
     print("Done.")
 
 
-_encoding = 'utf-8'
-_output_directory = ''
-_one_output_per_file = False
+# Parse options and arguments
 opt_parser = optparse.OptionParser()
-opt_parser.add_option('-e', '--enc', dest='encoding',
-                      help='Use specific encoding for files. Default is utf-8.', metavar='ENC')
-opt_parser.add_option('-d', dest='directory',
-                      help='Output XML-file to specific directory.', metavar='DIR')
+opt_parser.add_option('-e', '--enc', dest='encoding', default='utf-8',
+                      help='Use specific encoding for files', metavar='ENC')
+opt_parser.add_option('-d', dest='directory', default='',
+                      help='Output XML-file to specific directory', metavar='DIR')
 opt_parser.add_option('--file-only', action='store_true', dest='file_only', default=False,
-                      help='Generate only one xml per file')
+                      help='Generate only overall xml for each file')
+opt_parser.add_option('--xslt', dest='xslt', default='',
+                      help='XSLT file to transform XML to desired format', metavar='XSLT')
 options, args = opt_parser.parse_args()
 
-if options.encoding:
-    try:
-        codecs.lookup(options.encoding)
-    except LookupError:
-        print("Encoding " + options.encoding + " is not supported!")
-        exit(1)
-    _encoding = options.encoding
+# Verify that options are correct
+try:
+    codecs.lookup(options.encoding)
+except LookupError:
+    print("Encoding " + options.encoding + " is not supported!")
+    exit(1)
+_encoding = options.encoding
 if options.directory:
     if not os.path.isdir(options.directory):
         print("Directory '" + options.directory + "' doesn't exists")
         exit(1)
-    _output_directory = options.directory
-    _output_directory = 'DEFAULT ([file-dir]/cstruct2xml-output/)'
-if options.file_only:
-    _one_output_per_file = True
+if options.xslt:
+    xsl = et.parse(options.xslt)
+    transform = et.XSLT(xsl)  # XSL transformation function
 
-print("Using encoding: " + _encoding)
+print("Using encoding: " + options.encoding)
 print("Using output directory: " +
-      (_output_directory if _output_directory else 'DEFAULT ([file-dir]/cstruct2xml-output/)'))
-if _one_output_per_file:
+      (options.directory if options.directory else 'DEFAULT ([file-dir]/cstruct2xml-output/)'))
+if options.file_only:
     print("Generating one output xml per file")
 else:
     print("Generating xml file for each structure + overall for file")
-
-if len(args) == 0:
-    print("No files provided, exiting.")
-    exit(0)
 
 for arg in args:
     if os.path.isdir(arg):
